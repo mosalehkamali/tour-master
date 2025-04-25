59618d6a175ab63dbb165bc66d4c5c66235a210c
mosalehkamali <mosalehkamali@gmail.com>
2025-04-25 07:16:23 +0430

    fix bugs  and change database

--- src/app/p-admin/layout.js
+++ src/app/p-admin/layout.js
@@ -1,24 +1,26 @@
-"use client"
+"use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function RootLayout({ children }) {
-    const router = useRouter();
-        const cookies = document.cookie.split(';').reduce((acc, cookie) => {
-          const [key, value] = cookie.trim().split('=');
-          acc[key] = value;
-          return acc;
-        }, {});
-    
-        if (!cookies.authToken) {
-          router.push('/p-admin/admin-auth');
-        }
-      
+  const router = useRouter();
+
+  useEffect(() => {
+    const cookies = document.cookie.split(";").reduce((acc, cookie) => {
+      const [key, value] = cookie.trim().split("=");
+      acc[key] = value;
+      return acc;
+    }, {});
+
+    if (!cookies.authToken) {
+      router.push("/p-admin/admin-auth");
+    }
+  }, []);
+
   return (
     <html lang="fa">
-      <body suppressHydrationWarning={true}>
-        {children}
-      </body>
+      <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
 }
