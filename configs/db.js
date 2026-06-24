9a03acafcf9ce420952a24cd1c98e51a5459e843
mosalehkamali <mosalehkamali@gmail.com>
2026-06-24 14:01:44 +0330

    connect to db

--- configs/db.js
+++ configs/db.js
@@ -7,7 +7,7 @@ const connectToDB = async () => {
       // await mongoose.connect("mongodb://localhost:27017/Kerman-Tour");
       
       // mongoDb Atlas 
-      await mongoose.connect("mongodb+srv://mosalehk7:Mk3020566509@cluster0.sshzl77.mongodb.net/?appName=Cluster0");
+      await mongoose.connect("mongodb+srv://mohisalehkamali_db_user:S1meM5ebiqowuc8W@cluster0.k0kq5ej.mongodb.net");
       
       // Host database
       // await mongoose.connect("mongodb://mohiabad_mosalehk:Mk3020566509@localhost:27017/mohiabad_tourDB");
